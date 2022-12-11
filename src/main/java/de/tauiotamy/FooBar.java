37941da383645af3fce198959205ca2d812ad088
Tim <116430482+TimNrw1979@users.noreply.github.com>
2022-12-11 12:08:52 +0100

    Logging aufgenommen

--- src/main/java/de/tauiotamy/FooBar.java
+++ src/main/java/de/tauiotamy/FooBar.java
@@ -1,16 +1,31 @@
 package de.tauiotamy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Demo class.
  */
 public class FooBar {
 
-  /**
-   * Dummy method.
-   * @return 0.
-   */
-  public int dummy() {
-    return 0;
-  }
+	/**
+	 * Logger.
+	 */
+	private static Logger LOG = LoggerFactory.getLogger(FooBar.class);
+
+	/**
+	 * Dummy method.
+	 * 
+	 * @return 0.
+	 */
+	public int dummy() {
+		LOG.error("Dummy");
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		LOG.info("Test");
+		System.out.println(new FooBar().dummy());
+	}
 
 }
