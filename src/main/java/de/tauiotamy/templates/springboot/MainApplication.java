3605f853700f37121b8805b52081e97704cc4d6a
Tim <116430482+TimNrw1979@users.noreply.github.com>
2022-12-17 09:06:31 +0100

    Logging aufgenommen

--- src/main/java/de/tauiotamy/templates/springboot/MainApplication.java
+++ src/main/java/de/tauiotamy/templates/springboot/MainApplication.java
@@ -1,5 +1,7 @@
 package de.tauiotamy.templates.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -17,11 +19,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class MainApplication {
 
 	/**
+	 * Logger.
+	 */
+	private static Logger LOG = LoggerFactory.getLogger(MainApplication.class);
+
+	/**
 	 * Einstiegsmethode.
 	 * 
 	 * @param args Kommandozeilenparameter.
 	 */
 	public static void main(String[] args) {
+		LOG.info("Starte Anwendung ...");
 		SpringApplication.run(MainApplication.class, args);
 	}
 }
